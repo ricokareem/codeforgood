7e7282844c432811f86c0f4c2f0c85ce233e35e7
Rico Rodriquez Collins <rico.collins@gmail.com>
2025-05-21 12:35:05 -0700

    Migrate config files to .mjs/.ts, update Tailwind and add next-themes dependency

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  reactStrictMode: true,
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en", "de", "es"],
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**.public.blob.vercel-storage.com",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
